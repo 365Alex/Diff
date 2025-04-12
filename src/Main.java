32309a9aa4f739c17e6765ec5afb9a6567a086cb
master <aleshenko_ira_2017@mail.ru>
2025-04-12 21:36:50 +0300

    gfygcc

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        var dog = 8.0;
         var cat = 3.6;
-        System.out.println(dog);
+        System.out.println(cat);
     }
 }
